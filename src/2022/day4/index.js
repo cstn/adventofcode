030c70de4fb1bd922fba8575b9d0df0c5fedcec4
Carsten Stein <carsten.stein@cast-it.de>
2022-12-14 18:47:42 +0100

    day four part two

--- src/2022/day4/index.js
+++ src/2022/day4/index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 
-const includes = (a, b) => a.from <= b.from && a.to >= b.to || a.from >= b.from && a.to <= b.to;
+const contains = (a, b) => a.from <= b.from && a.to >= b.to || a.from >= b.from && a.to <= b.to;
+const overlaps = (a, b) => contains(a, b) || a.from <= b.from && a.to >= b.from || a.from >= b.from && a.from <= b.to;
 
 const partOne = (filename = 'input.txt') => {
   const text = fs.readFileSync(`${__dirname}/${filename}`, 'utf-8');
@@ -13,10 +14,28 @@ const partOne = (filename = 'input.txt') => {
       { from: parseInt(a[0], 10), to: parseInt(a[1], 10)},
       { from: parseInt(b[0], 10), to: parseInt(b[1], 10)},
     ])
-    .filter((pair) => includes(...pair));
+    .filter((pair) => contains(...pair));
 
   // eslint-disable-next-line no-console
   console.log('Part 1: assignment pairs fully containing the other', result.length)
 };
 
+const partTwo = (filename = 'input.txt') => {
+  const text = fs.readFileSync(`${__dirname}/${filename}`, 'utf-8');
+
+  const result = text
+    .split(/\n/)
+    .map((line) => line.split(','))
+    .map(([a, b]) => [a.split('-'), b.split('-')])
+    .map(([a, b]) => [
+      { from: parseInt(a[0], 10), to: parseInt(a[1], 10)},
+      { from: parseInt(b[0], 10), to: parseInt(b[1], 10)},
+    ])
+    .filter((pair) => overlaps(...pair));
+
+  // eslint-disable-next-line no-console
+  console.log('Part 2: assignment pairs overlapping', result.length)
+};
+
 partOne('input.txt');
+partTwo('input.txt');
