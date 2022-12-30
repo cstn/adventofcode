35f55a1e38048fc1fee08e7d722295ddba0e9b54
Carsten Stein <carsten.stein@cast-it.de>
2022-12-30 12:33:17 +0100

    enable typescript

--- src/2015/day1/index.ts
+++ src/2015/day1/index.ts
@@ -1,25 +1,25 @@
-const fs = require('fs');
+import * as fs from 'fs';
 
-const walk = (directions, startLevel = 0) =>
+const walk = (directions: string[], startLevel = 0): number =>
   directions.reduce((acc, curr) => curr === '(' ? acc + 1 : acc - 1, startLevel);
 
-const goToBasement = (directions, startLevel = 0) => {
+const goToBasement = (directions: string[], startLevel = 0): number => {
   let level = startLevel;
 
-  return directions.findIndex((direction) => {
+  return directions.findIndex((direction: string) => {
     level = direction === '(' ? level + 1 : level - 1;
 
     return level < 0;
   }) + 1;
 }
 
-const partOne = (filename) => {
+const partOne = (filename: string) => {
   const input = fs.readFileSync(`${__dirname}/${filename}`, 'utf-8').split('');
 
   return walk(input);
 }
 
-const partTwo = (filename) => {
+const partTwo = (filename: string) => {
   const input = fs.readFileSync(`${__dirname}/${filename}`, 'utf-8').split('');
 
   return goToBasement(input);
