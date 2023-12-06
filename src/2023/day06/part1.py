e4385b0f8101e57588fda4f994e885d471876ee2
Carsten Stein <carsten.stein@cast-it.de>
2023-12-06 20:02:14 +0100

    day 6 part 1

---
+++ src/2023/day06/part1.py
@@ -0,0 +1,43 @@
+import numpy as np
+import re
+
+
+def read_input(name):
+    with open(name, 'r') as f:
+        lines = f.read().splitlines()
+    f.close()
+
+    return lines
+
+
+def calc_speed(button_down_time):
+    return button_down_time
+
+
+def calc_distance(total_time, button_down_time, speed):
+    return (total_time - button_down_time) * speed
+
+
+def main(filename):
+    read_lines = read_input(filename)
+    races = np.array([list(map(lambda x: int(x), re.split('\s+', line)[1:])) for line in read_lines])
+    print(races)
+
+    ways_to_win = 1
+    for i, time in enumerate(races[0]):
+        print('Time', time)
+        button_down_times = range(0, time + 1)
+        wins = []
+        for button_down_time in button_down_times:
+            speed = calc_speed(button_down_time)
+            distance = calc_distance(time, button_down_time, speed)
+            if distance > races[1, i]:
+                wins.append(button_down_time)
+        print('Wins', wins)
+        ways_to_win *= len(wins)
+    return ways_to_win
+
+
+# filename = 'sample.txt'
+filename = 'input.txt'
+print('Result', main(filename))
