f849bc157f142240b73887a2f25b3f9d63dcef1a
Carsten Stein <carsten.stein@cast-it.de>
2023-12-08 08:00:37 +0100

    2023 day 8 part 1

---
+++ src/2023/day08/part2.py
@@ -0,0 +1,26 @@
+import numpy as np
+import re
+
+
+def read_input(name):
+    with open(name, 'r') as f:
+        lines = f.read().splitlines()
+    f.close()
+
+    return lines
+
+
+def parse_input(lines):
+    return np.array([re.split(', ', line) for line in lines])
+
+
+def main(filename):
+    read_lines = read_input(filename)
+    parsed_lines = parse_input(read_lines)
+    print('Parsed', parsed_lines)
+    return 0
+
+
+filename = 'sample.txt'
+# filename = 'input.txt'
+print('Result', main(filename))
