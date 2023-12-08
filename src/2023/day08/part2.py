a0ab45ebe0af4088ce82b0e458e1549be3d32e2b
Carsten Stein <carsten.stein@cast-it.de>
2023-12-08 09:10:56 +0100

    2023 day 8 part 2

--- src/2023/day08/part2.py
+++ src/2023/day08/part2.py
@@ -1,4 +1,3 @@
-import numpy as np
 import re
 
 
@@ -11,16 +10,40 @@ def read_input(name):
 
 
 def parse_input(lines):
-    return np.array([re.split(', ', line) for line in lines])
+    instructions = lines[0]
+    nodes = [re.split(' = \(|, |\)', line)[:3] for line in lines[2:]]
+    return {
+        'instructions': instructions,
+        'nodes': nodes
+    }
+
+
+def end_position(positions):
+    for p in positions:
+        if p[-1] != 'Z':
+            return False
+    return True
 
 
 def main(filename):
     read_lines = read_input(filename)
     parsed_lines = parse_input(read_lines)
-    print('Parsed', parsed_lines)
-    return 0
+    instructions = parsed_lines['instructions']
+    nodes = parsed_lines['nodes']
+    starts = list(map(lambda n: n[0], filter(lambda n: n[0].endswith('A'), nodes)))
+    positions = starts
+    steps = 0
+    while not end_position(positions):
+        for i, turn in enumerate(instructions):
+            steps += 1
+            for p, position in enumerate(positions):
+                node = next(filter(lambda n: n[0] == position, nodes))
+                if turn == 'L':
+                    positions[p] = node[1]
+                elif turn == 'R':
+                    positions[p] = node[2]
+    return steps
 
 
-filename = 'sample.txt'
-# filename = 'input.txt'
-print('Result', main(filename))
+print('Result', main('sample2.txt'))
+print('Result', main('input.txt'))
