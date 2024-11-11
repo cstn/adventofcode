00f8bdd8a52b076de4067afd1811d15fae26bc93
Carsten Stein <carsten.stein@cast-it.de>
2024-11-11 18:32:38 +0100

    Initial project

---
+++ template/part2.py
@@ -0,0 +1,25 @@
+import numpy as np
+import re
+
+
+def read_input(name):
+    with open(name, 'r') as f:
+        lines = f.read().splitlines()
+    f.close()
+
+    return lines
+
+
+def parse_input(lines):
+    return np.array([re.split(', ', line) for line in lines])
+
+
+def main(filename):
+    read_lines = read_input(filename)
+    parsed_lines = parse_input(read_lines)
+    print('Parsed', parsed_lines)
+    return 0
+
+
+print('Sample result', main('sample.txt'))
+print('Main result', main('input.txt'))
